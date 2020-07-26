8b2ebfc0f36d866abf7f30d848e2ad2a80e040f6
Deep Majumder <deep.majumder2019@gmail.com>
2020-07-26 22:30:39 +0530

    Moved around code a bit

--- src/bin/full_board.rs
+++ src/bin/full_board.rs
@@ -1,4 +1,4 @@
-use nqueens::columns::*;
+use nqueens::board::*;
 use std::env::args;
 
 fn main() {
@@ -8,37 +8,37 @@ fn main() {
   } else {
     size = 8;
   }
-  let (moves, tries, solution) = column_restart_hill_climb(size);
+  let (moves, tries, solution) = restart_hill_climb(size);
   assert!(solution.is_goal());
   println!("{}", solution);
   println!("In {} moves", moves);
   println!("In {} restarts", tries);
 }
 
-fn column_restart_hill_climb(size: usize) -> (i32, i64, ColumnBoard) {
-  let mut board = ColumnBoard::new(size);
+fn restart_hill_climb(size: usize) -> (i32, i64, Board) {
+  let mut board = Board::randomly_filled_board(size);
   let mut tries = 0;
   loop {
-    let (moves, solution) = column_hill_climb(board);
+    let (moves, solution) = hill_climb(board);
     tries += 1;
     if let Some(solution) = solution {
       return (moves, tries, solution);
     }
-    board = ColumnBoard::new(size);
+    board = Board::randomly_filled_board(size);
   }
 }
 
-fn column_hill_climb(board: ColumnBoard) -> (i32, Option<ColumnBoard>) {
+fn hill_climb(board: Board) -> (i32, Option<Board>) {
   let mut cnt = 0;
   let mut board = board;
   loop {
-    let current_heuristic = board.heuristic();
+    let current_heuristic = board.attacking_heuristic();
     let next_board = board.best_neighbor();
     cnt += 1;
     if next_board.is_goal() {
       return (cnt, Some(next_board));
     }
-    if next_board.heuristic() == current_heuristic {
+    if next_board.attacking_heuristic() == current_heuristic {
       return (cnt, None);
     }
     board = next_board;
